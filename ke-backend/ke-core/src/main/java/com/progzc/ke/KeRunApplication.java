0eff2a7ee1fe524ef8f6527ae51a8a2bb02aa285
progzc <zcprog@foxmail.com>
2021-03-16 16:38:11 +0800

    后台模块初始化

---
+++ ke-backend/ke-core/src/main/java/com/progzc/ke/KeRunApplication.java
@@ -0,0 +1,18 @@
+package com.progzc.ke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description 课程监控系统统计启动类
+ * @Author zhaochao
+ * @Date 2021/3/16 14:59
+ * @Email zcprog@foxmail.com
+ * @Version V1.0
+ */
+@SpringBootApplication
+public class KeRunApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KeRunApplication.class, args);
+    }
+}
---
+++ ke-backend/ke-core/src/test/java/com/progzc/ke/KeTestApplication.java
@@ -0,0 +1,17 @@
+package com.progzc.ke;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @Description 课程监控系统统计测试类
+ * @Author zhaochao
+ * @Date 2021/3/16 15:04
+ * @Email zcprog@foxmail.com
+ * @Version V1.0
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KeTestApplication {
+}

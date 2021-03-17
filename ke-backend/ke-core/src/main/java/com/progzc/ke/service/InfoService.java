fbb91ce459b87b839ccf10cf843e1e621be8b8be
progzc <zcprog@foxmail.com>
2021-03-17 15:45:00 +0800

    服务接口

---
+++ ke-backend/ke-core/src/main/java/com/progzc/ke/service/InfoService.java
@@ -0,0 +1,14 @@
+package com.progzc.ke.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.progzc.ke.entity.Info;
+
+/**
+ * @Description Info服务接口
+ * @Author zhaocho
+ * @Date 2021/3/17 15:41
+ * @Email zcprog@foxmail.com
+ * @Version V1.0
+ */
+public interface InfoService extends IService<Info> {
+}
---
+++ ke-backend/ke-core/src/main/java/com/progzc/ke/service/MenuService.java
@@ -0,0 +1,14 @@
+package com.progzc.ke.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.progzc.ke.entity.Menu;
+
+/**
+ * @Description Menu服务接口
+ * @Author zhaocho
+ * @Date 2021/3/17 15:40
+ * @Email zcprog@foxmail.com
+ * @Version V1.0
+ */
+public interface MenuService extends IService<Menu> {
+}

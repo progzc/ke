dba33a95c7abc81e2b0cb9e5fb01a4d1feee6be0
progzc <zcprog@foxmail.com>
2021-03-17 17:56:48 +0800

    控制器

---
+++ ke-backend/ke-core/src/main/java/com/progzc/ke/controller/CourseController.java
@@ -0,0 +1,14 @@
+package com.progzc.ke.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description 课程销售信息控制器
+ * @Author zhaochao
+ * @Date 2021/3/17 17:54
+ * @Email zcprog@foxmail.com
+ * @Version V1.0
+ */
+@RestController
+public class CourseController {
+}
---
+++ ke-backend/ke-core/src/main/java/com/progzc/ke/controller/MenuController.java
@@ -0,0 +1,14 @@
+package com.progzc.ke.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description 菜单控制器
+ * @Author zhaochao
+ * @Date 2021/3/17 17:52
+ * @Email zcprog@foxmail.com
+ * @Version V1.0
+ */
+@RestController
+public class MenuController {
+}

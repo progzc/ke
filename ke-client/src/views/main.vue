0236a61f94de7745417fbe01548d3a830a399e1e
progzc <zcprog@foxmail.com>
2021-03-18 18:46:52 +0800

    add main.vue

---
+++ ke-client/src/views/main.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="site-wrapper">
+      <main-navbar></main-navbar>
+      <main-sidebar></main-sidebar>
+      <div class="site-content-wrapper">
+        <main-content></main-content>
+      </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+import MainNavbar from 'views/layout/MainNavbar'
+import MainSidebar from 'views/layout/MainSidebar'
+import MainContent from 'views/layout/MainContent'
+
+export default {
+  name: 'main',
+  components: {
+    'main-navbar': MainNavbar,
+    'main-sidebar': MainSidebar,
+    'main-content': MainContent
+  }
+}
+</script>
+
+<style lang="scss" type="text/scss" rel="stylesheet/scss" scoped>
+
+</style>

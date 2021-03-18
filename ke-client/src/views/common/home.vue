b42d2ec2e6745ba6eb9a2bdcbdb36735f168b19b
progzc <zcprog@foxmail.com>
2021-03-18 19:30:55 +0800

    add home.vue

---
+++ ke-client/src/views/common/home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class = "home-content">
+    <h3>腾讯课堂监控统计系统</h3>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: 'home'
+}
+</script>
+
+<style lang="scss" type="text/scss" rel="stylesheet/scss" scoped>
+
+</style>

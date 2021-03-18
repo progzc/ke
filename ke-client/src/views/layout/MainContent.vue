4ff6008f1e4c7278e9f3b46bbc5c9be53a1136c3
progzc <zcprog@foxmail.com>
2021-03-18 19:15:46 +0800

    add MainContent.vue

---
+++ ke-client/src/views/layout/MainContent.vue
@@ -0,0 +1,18 @@
+<template>
+  <main class="site-content">
+    <router-view/>
+  </main>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: 'MainContent',
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss" type="text/scss" rel="stylesheet/scss" scoped>
+
+</style>

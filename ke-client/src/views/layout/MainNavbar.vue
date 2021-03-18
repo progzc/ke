57b7447ace2bc139ceea192803f70176f002431f
progzc <zcprog@foxmail.com>
2021-03-18 19:00:22 +0800

    add MainNavbar.vue

---
+++ ke-client/src/views/layout/MainNavbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav class="site-navbar">
+    <div class="site-navbar-header">
+      <h1 class="site-navbar-brand" @click="$router.push({ name: 'home' })">
+        <span class="iconfont icon-ke">&#xea44;</span>
+        <a class="site-navbar-brand-name" href="javascript:;">Monitor System for Ke of QQ</a>
+      </h1>
+    </div>
+  </nav>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: 'MainNavbar'
+}
+</script>
+
+<style lang="scss" type="text/scss" rel="stylesheet/scss" scoped>
+
+</style>

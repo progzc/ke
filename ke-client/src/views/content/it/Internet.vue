ad1fd2d3fd3ca7b3a2ba8c194a761807a3aa4290
progzc <zcprog@foxmail.com>
2021-03-18 19:32:10 +0800

    add Internet.vue

---
+++ ke-client/src/views/content/it/Internet.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class = "internet-content">
+    <h3>IT互联网课程监控</h3>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: 'Internet'
+}
+</script>
+
+<style lang="scss" type="text/scss" rel="stylesheet/scss" scoped>
+
+</style>

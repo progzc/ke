4f07b8a170fed9174fa3a258f5c2ca6f6b44bc44
progzc <zcprog@foxmail.com>
2021-03-19 20:59:38 +0800

    完成折线图的绘制

---
+++ ke-backend/ke-core/src/main/java/com/progzc/ke/constants/Category.java
@@ -0,0 +1,20 @@
+package com.progzc.ke.constants;
+
+/**
+ * @Description 分类查询
+ * @Author zhaochao
+ * @Date 2021/3/19 18:02
+ * @Email zcprog@foxmail.com
+ * @Version V1.0
+ */
+public class Category {
+    /**
+     * 按照售出情况进行查询
+     */
+    public final static int SELLING = 1;
+
+    /**
+     * 按照浏览情况进行查询
+     */
+    public final static int VIEW = 2;
+}

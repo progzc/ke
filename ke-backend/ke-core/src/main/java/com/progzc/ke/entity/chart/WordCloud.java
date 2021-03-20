b60e0b38116c4cc85e047effd9e5a3c62e0e3433
progzc <zcprog@foxmail.com>
2021-03-20 09:18:52 +0800

    封装词云结果

---
+++ ke-backend/ke-core/src/main/java/com/progzc/ke/entity/chart/WordCloud.java
@@ -0,0 +1,26 @@
+package com.progzc.ke.entity.chart;
+
+import io.swagger.annotations.ApiModel;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * @Description 词云
+ * @Author zhaochao
+ * @Date 2021/3/20 9:13
+ * @Email zcprog@foxmail.com
+ * @Version V1.0
+ */
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+@ApiModel(value = "WordCloud对象", description = "封装词云结果")
+public class WordCloud implements Serializable {
+    private static final long serialVersionUID = -247629682917624014L;
+
+    private String name;
+    private Integer value;
+}

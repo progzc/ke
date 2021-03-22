1cd2dc8b33b6fb46e3661e8d0bafbc145ade5b69
progzc <zcprog@foxmail.com>
2021-03-22 21:08:43 +0800

    完善异常处理

--- ke-backend/ke-core/src/main/java/com/progzc/ke/common/enums/ErrorEnum.java
+++ ke-backend/ke-core/src/main/java/com/progzc/ke/common/enums/ErrorEnum.java
@@ -16,7 +16,8 @@ public enum ErrorEnum {
     // 请求错误
     PATH_NOT_FOUND(404, "路径不存在，请检查路径"),
     // 系统错误
-    UNKNOWN(500, "系统内部错误，请联系管理员");
+    UNKNOWN(500, "系统内部错误，请联系管理员"),
+    DUPLICATE_KEY(501, "数据库中已存在该记录");
 
     private int code;
     private String msg;
--- ke-backend/ke-core/src/main/java/com/progzc/ke/common/exception/MyExceptionHandler.java
+++ ke-backend/ke-core/src/main/java/com/progzc/ke/common/exception/MyExceptionHandler.java
@@ -1,7 +1,12 @@
 package com.progzc.ke.common.exception;
 
+import com.progzc.ke.common.Result;
+import com.progzc.ke.common.enums.ErrorEnum;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 /**
  * @Description 异常处理类
@@ -14,5 +19,49 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @Slf4j
 public class MyExceptionHandler {
     
+    /**
+     * 处理自定义异常：包含校验异常、认证异常
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(MyException.class)
+    public Result handleMyException(MyException e) {
+        Result result = new Result();
+        result.put("code", e.getCode());
+        result.put("msg", e.getMsg());
+        return result;
+    }
 
+    /**
+     * 处理路径找不到的异常
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public Result hanlerNoFoundException(NoHandlerFoundException e) {
+        log.error(e.getMessage(), e);
+        return Result.exception(ErrorEnum.PATH_NOT_FOUND);
+    }
+
+    /**
+     * 处理DAO异常
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(DuplicateKeyException.class)
+    public Result handleDuplicateKeyException(DuplicateKeyException e) {
+        log.error(e.getMessage(), e);
+        return Result.exception(ErrorEnum.DUPLICATE_KEY);
+    }
+
+    /**
+     * 处理Exception异常
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(Exception.class)
+    public Result handleException(Exception e) {
+        log.error(e.getMessage(), e);
+        return Result.exception();
+    }
 }
